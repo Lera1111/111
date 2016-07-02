fe9c52dd76d02a368ca5bf9c37baae704dc2fc9b
Lera1111 <lerikakau@gmail.com>
2016-07-02 19:18:11 +0500

    111

---
+++ alarm/obj/Debug/android/src/md5e2b13797e09cdb7fde346cd95c4196a0/edit.java
@@ -0,0 +1,47 @@
+package md5e2b13797e09cdb7fde346cd95c4196a0;
+
+
+public class edit
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("alarm.edit, alarm, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", edit.class, __md_methods);
+	}
+
+
+	public edit () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == edit.class)
+			mono.android.TypeManager.Activate ("alarm.edit, alarm, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
